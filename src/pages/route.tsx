6d7bb3989db6c49285f6515ebe7a43de51f19133
IRZYNI <137916351+IRZYNI@users.noreply.github.com>
2025-04-17 20:28:26 +0200

    Delete src/pages/route.tsx

--- src/pages/route.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/tutorial.tsx
-export default function Tutorial() {
-  return (
-    <div class="p-4">
-      <h1 class="text-2xl font-bold">Tutorial Page</h1>
-      <p>This is your tutorial content.</p>
-    </div>
-  );
-}
